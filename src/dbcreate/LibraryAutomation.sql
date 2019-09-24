088574303276116e1ae77303b1bec71b45ed739f
heartfreeforever <1765578099@qq.com>
2019-09-24 18:24:00 +0800

    更新sql

--- src/dbcreate/LibraryAutomation.sql
+++ src/dbcreate/LibraryAutomation.sql
@@ -1,4 +1,6 @@
 use libraryautomation;
+-- insert into `book` VALUE('1','hlm','12','qq','3_dong','12341231','avaliable','science');
+-- insert into `book` VALUE('2','xyj','13','ww','3_dong','12341232','avaliable','sciadadaw');
 create table book(
 	BookId varchar(20) not null,
     BookName varchar(20) not null,
@@ -6,27 +8,48 @@ create table book(
     Price varchar(20) not null,
     Position varchar(20) not null,
     ISBN varchar(20) not null,
-    Status varchar(20) not null, --avaliable
+    status varchar(20) not null, -- available/reserved/lent
     Category varchar(20) not null,
     PRIMARY KEY (BookId)
 );
-
-create table record(
-	recordId varchar(20) not null,
-    BookName varchar(20) not null,
+-- insert into `bookrecord` VALUE('1',12,'1221-2-2 23:11;11','1222-1-1 2:11;11','2','0001');
+create table bookrecord(
+	informationId varchar(20) not null,
+    FineValue int not null,
+    LentDate DATETIME not null,
+    ReturnDate DATETIME not null,
+    BookId varchar(20) not null,
     AcntNum varchar(20) not null,
-    FineValue varchar(20) not null,
-    LentDate varchar(20) not null,
-    ReturnDate varchar(20) not null,
-    Status varchar(20) not null, --reserved or lent
-    PRIMARY KEY (recordId)
+    PRIMARY KEY (informationId)
+    
 );
-
+create table optionRecord(
+	informationId varchar(20) not null,
+	BookId varchar(20) not null,
+    AcntNum varchar(20) not null,
+	reason  varchar(255),
+	operation varchar(255) not null,
+	creationDate DATETIME not null,
+	PRIMARY KEY (informationId)
+	
+	
+);
+create table reserve(
+	informationId varchar(20) not null,
+	BookId varchar(20) not null,
+    AcntNum varchar(20) not null,
+	createDate DATETIME not null,
+	PRIMARY KEY (informationId)
+);
+-- insert into `reader` VALUE('0001','123456','1765578099@qq.com',1,'1220-2-2 5:12;13',300);
+-- insert into `reader` VALUE('0002','123456','1765513131@qq.com',2,'1220-2-1 5:12;13',300);
 create table reader(
 	AcntNum varchar(20) not null,
     Password varchar(20) not null,
     Email varchar(20) not null,
-    bookNumber int not null, --count lent books (max 3)
+    bookNumber int not null,
+    registerDate  DATETIME not null,
+    Deposit int not null,
     PRIMARY KEY(AcntNum)
 );
 
@@ -44,13 +67,33 @@ create table admin(
 
 create table announcement(
 	TextId varchar(20) not null,
-    Text varchar(1024) not null,
+    Text text not null,
     PRIMARY KEY(TextId)
 );
 
-create table finevalue(
-	recordId varchar(20) not null,
-	bookId varchar(20) not null,
-	acntNumber varchar(20) not null,
-	PRIMARY KEY(recordId)
-); 
+-- reader/book_bookrecord外键连接，关联更新，不关联删除
+-- alter table bookrecord
+-- add constraint reader_bookrecord foreign key (`acntnum`)
+-- references reader(`acntnum`) on update cascade on delete no action;
+
+-- alter table bookrecord
+-- add constraint book_bookrecord foreign key (`bookId`)
+-- references book(`bookId`) on update cascade on delete no action;
+
+-- reader/book_optionRecord外键连接，关联更新，关联删除
+alter table optionRecord
+add constraint reader_optionRecord foreign key (`acntnum`)
+references reader(`acntnum`) on update cascade on delete CASCADE;
+
+alter table optionRecord
+add constraint book_optionRecord foreign key (`bookId`)
+references book(`bookId`) on update cascade on delete CASCADE;
+
+-- reader/book_reserve外键连接，关联更新，关联删除
+alter table reserve
+add constraint reader_reserve foreign key (`acntnum`)
+references reader(`acntnum`) on update cascade on delete CASCADE;
+
+alter table reserve
+add constraint book_reserve foreign key (`bookId`)
+references book(`bookId`) on update cascade on delete CASCADE;
